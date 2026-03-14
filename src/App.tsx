94d1d7d77100166be33665e3f2e1a7030e3ee7b1
Luo Hui <lhui0123456@gmail.com>
2026-03-15 00:38:33 +0800

    fix(deploy): use HashRouter and set base path for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { BottomNav } from "./components/BottomNav";
 import { Home } from "./pages/Home";
